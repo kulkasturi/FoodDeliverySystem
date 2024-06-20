5d7e9499377e5a0534d9baf5e18375ce48a4c94d
PrathmeshK1905 <Kprathmesh619@gmail.com>
2024-06-21 02:03:45 +0530

    Created pages dr and all files in it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import Placeorder from './pages/PlaceOrder/Placeorder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/> 
-      
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<Placeorder/>} />
+      </Routes>
     </div>
   )
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+
 const Navbar = () => {
+
+const[menu,setMenu] = useState("menu");
+
   return (
     <div className='navbar'>
         <img src={assets.logo} alt="" className="logo" />
         <ul className="navbar-menu">
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Mobile-App</li>
-            <li>Contact us</li>
+            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+            <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+            <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
+            <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact us</li>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
