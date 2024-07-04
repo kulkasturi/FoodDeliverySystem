4ca668194b05d4ab49807e1082c1428c4630c067
Kasturi <kasturi_kulkarni_comp@moderncoe.edu.in>
2024-07-04 23:59:36 +0530

    Add Food Display and Food Item components

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
 
 
@@ -11,7 +12,11 @@ const FoodDisplay = ({category}) => {
   return (
     <div className='food_display' id='food_display'>
         <h2>Top Dishes near you</h2>
-      
+        <div className="food-display-list">
+            {food_list.map((item,index)=> {
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+            })}
+        </div>
     </div>
   )
 }
