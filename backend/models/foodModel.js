80354f3f6e8c409316cc2e484266005ba45dcb82
PrathmeshK1905 <Kprathmesh619@gmail.com>
2024-07-19 12:24:49 +0530

    Updated backend

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-constfoodSchema = new mongoose.Schema({
+const foodSchema = new mongoose.Schema({
     name: {type:String,required:true},
-    description: {type:Number,required:true},
+    description: {type:String,required:true},
     price: {type:Number,required:true},
     image: {type:String,required:true},
-    categogy: {type:String,required:true}
+    category: {type:String,required:true}
 })
 
 const foodModel = mongoose.models.food || mongoose.model("food",foodSchema);
